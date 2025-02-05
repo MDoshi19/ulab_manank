c81c91d78eda8e71c98315b2e8e46e7992e0cc6c
Manank Doshi <manank@wifi-10-41-78-4.wifi.berkeley.edu>
2025-02-05 12:41:51 -0800

    Did homework 12

--- homework11/keplerthird.py
+++
@@ -1,4 +0,0 @@
-def kepler_third_time_from_semimajor(semimajor_axis):
-    # Returns the orbital period based on semimajor axis using Kepler's Third Law
-    orbital_period = (semimajor_axis**3)**0.5
-    return orbital_period
